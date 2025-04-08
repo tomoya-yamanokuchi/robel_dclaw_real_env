8509d403ded85fa227a9769d885d1675bda04a0c
tomoya-y <yamanokuchi.tomoya.ys9@is.naist.jp>
2025-04-08 12:37:53 +0900

    add

--- test/test_env_reset_case1.py
+++ test/test_env_reset_case1.py
@@ -13,7 +13,7 @@ def run_tests():
     task_space_ctrl_init     = Manifold1D(value=task_space_position_init.reshape(1,1,3))
     # ----
     env = RobelDClawValveRealEnvironment()
-    env.reset(task_space_ctrl_init, valve_position_init=0)
+    env.reset(task_space_ctrl_init, valve_position_init=np.pi)
     # ----
 
 if __name__ == "__main__":
